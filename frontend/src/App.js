e095348bf36954195154b782dc2c0fed3144665f
soundtemple <twwalter@gmail.com>
2020-02-25 10:29:40 +1100

    setup flash message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Dashboard from "./app/components/dashboard";
 import "./App.css";
+import FlashMessage from "./utils/flash-message";
 
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
@@ -12,12 +13,20 @@ const theme = createMuiTheme({
 });
 
 const App = props => {
+  const [flashContent, setFlashContent] = useState();
+  const [showFlash, setShowFlash] = useState(false);
+  const handleFlashMessage = flashContent => {
+    setShowFlash(false);
+    setFlashContent(flashContent);
+    setShowFlash(true);
+  };
   return (
     <div className="App">
       <MuiThemeProvider theme={theme}>
+        {showFlash && <FlashMessage {...flashContent} />}
         <div>
           <CssBaseline />
-          <Dashboard />
+          <Dashboard onFlash={handleFlashMessage} />
         </div>
       </MuiThemeProvider>
     </div>
