8f4b230276c191b540c224b5ea3f96c7c6a17aec
soundtemple <twwalter@gmail.com>
2020-05-26 10:47:15 +1000

    show hide appropriate login buttons

--- frontend/src/auth/components/signin.js
+++ frontend/src/auth/components/signin.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "../../utils/axios-wrapper";
 import { Formik, Form, Field } from "formik";
 import * as yup from "yup"; // for everything
@@ -7,55 +7,55 @@ import Button from "@material-ui/core/Button";
 
 // Login form with Formik and Yup
 const SignInSchema = yup.object().shape({
-  email: yup
-    .string()
-    .email("Not a valid email")
-    .required("Email is required"),
+  email: yup.string().email("Not a valid email").required("Email is required"),
   password: yup
     .string()
     .required("C'mon password is required")
     .min(3, "Must be > 3 characters")
-    .max(12, "Must be <= 12 characters")
+    .max(12, "Must be <= 12 characters"),
 });
 
-function handleLogout() {
-  const url = "http://localhost:9000/auth/token/logout/";
-  axios
-    .post(url)
-    .then(resp => {
-      localStorage.removeItem("token");
-      console.log("LOGGED OUT");
-    })
-    .catch(error => {
-      console.log(`ERRORS: ${error}`);
-    });
-}
-
-function handleShowUser() {
-  const url = "http://localhost:9000/auth/users/me/";
-  axios
-    .get(url)
-    .then(resp => {
-      console.log(resp.data);
-      console.log(`USER: ${resp}`);
-    })
-    .catch(error => {
-      console.log(`ERRORS: ${error}`);
-    });
-}
-function handleForgotPassword() {
-  // route to forgot password form
-  console.log("Routing to forgot password form");
-}
-
-const SignIn = props => {
+const SignIn = (props) => {
   const { onFlash } = props;
+  const [loggedIn, setLoggedIn] = useState(localStorage["token"]);
+
+  function handleLogout() {
+    const url = "http://localhost:9000/auth/token/logout/";
+    axios
+      .post(url)
+      .then((resp) => {
+        localStorage.removeItem("token");
+        console.log("LOGGED OUT");
+        setLoggedIn(false);
+      })
+      .catch((error) => {
+        console.log(`ERRORS: ${error}`);
+      });
+  }
+
+  function handleShowUser() {
+    const url = "http://localhost:9000/auth/users/me/";
+    axios
+      .get(url)
+      .then((resp) => {
+        console.log(resp.data);
+        console.log(`USER: ${resp}`);
+      })
+      .catch((error) => {
+        console.log(`ERRORS: ${error}`);
+      });
+  }
+  function handleForgotPassword() {
+    // route to forgot password form
+    console.log("Routing to forgot password form");
+  }
+
   return (
     <>
       <Formik
         initialValues={{
           email: "",
-          password: ""
+          password: "",
         }}
         validationSchema={SignInSchema}
         validateOnBlur={true}
@@ -64,12 +64,13 @@ const SignIn = props => {
           const url = "http://localhost:9000/auth/token/login/";
           axios
             .post(url, values)
-            .then(resp => {
+            .then((resp) => {
               localStorage.setItem("token", resp.data.auth_token);
               console.log(`LOGGED IN: token ${resp.data.auth_token}`);
+              setLoggedIn(resp.data.auth_token);
               // window.location.reload(); or redirect somewhere???
             })
-            .catch(error => {
+            .catch((error) => {
               if (error.response && error.response.status === 400) {
                 let message = "There was an error processing your request";
                 if (error.response.data.non_field_errors) {
@@ -77,45 +78,52 @@ const SignIn = props => {
                 }
                 onFlash({
                   severity: "error",
-                  message: message
+                  message: message,
                 });
                 actions.setErrors({ email: message });
+                setLoggedIn(false);
               } else {
                 onFlash({
                   severity: "error",
-                  message: "There was a problem processing your request"
+                  message: "There was a problem processing your request",
                 });
               }
               actions.setSubmitting(false);
             });
         }}
       >
-        {// these goodies are passed in a Formik component:
-        //  - values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting
-        // we can add to an input field manually like onChange={handleBlur}
-        // in a Formik Field they are passed already in the formik object  like my input component
-        // TextField component extracts the repititive aspects of the formik Field Component
-        ({ isValid, errors, touched }) => (
-          <Form>
-            <h3>Sign in</h3>
-            <Field name="email" label="Email" component={TextField} />
-            <Field
-              name="password"
-              label="Password"
-              type="password"
-              component={TextField}
-              autoComplete="off"
-            />
-            <Button disabled={!isValid} type="submit">
-              Sign in
-            </Button>
-            {/* <Debug /> */}
-          </Form>
-        )}
+        {
+          // these goodies are passed in a Formik component:
+          //  - values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting
+          // we can add to an input field manually like onChange={handleBlur}
+          // in a Formik Field they are passed already in the formik object  like my input component
+          // TextField component extracts the repititive aspects of the formik Field Component
+          ({ isValid, errors, touched }) => (
+            <Form>
+              <h3>Sign in</h3>
+              <Field name="email" label="Email" component={TextField} />
+              <Field
+                name="password"
+                label="Password"
+                type="password"
+                component={TextField}
+                autoComplete="off"
+              />
+              {!loggedIn && (
+                <Button disabled={!isValid} type="submit">
+                  Sign in
+                </Button>
+              )}
+              {/* <Debug /> */}
+            </Form>
+          )
+        }
       </Formik>
-      <Button onClick={() => handleLogout()}>Logout</Button>
-      <Button onClick={() => handleForgotPassword()}>Forgot password</Button>
-      <Button onClick={() => handleShowUser()}>Show User</Button>
+      {loggedIn && <Button onClick={() => handleLogout()}>Logout</Button>}
+      {!loggedIn && (
+        <Button onClick={() => handleForgotPassword()}>Forgot password</Button>
+      )}
+      {loggedIn && <Button onClick={() => handleShowUser()}>Show User</Button>}
     </>
   );
 };
