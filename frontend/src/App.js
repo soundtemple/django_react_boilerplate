f012e1324177ece9a50ea426b6a898b5f00a8496
soundtemple <twwalter@gmail.com>
2020-05-28 18:12:15 +1000

    setup AppContext

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,34 +2,51 @@ import React, { useState } from "react";
 import Dashboard from "./app/components/dashboard";
 import "./App.css";
 import FlashMessage from "./utils/flash-message";
+import AppContext from "./app/components/app-context";
 
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
   palette: {
-    type: "light"
-  }
+    type: "light",
+  },
 });
 
-const App = props => {
+const App = (props) => {
   const [flashContent, setFlashContent] = useState();
   const [showFlash, setShowFlash] = useState(false);
-  const handleFlashMessage = flashContent => {
+  const handleFlashMessage = (flashContent) => {
     setShowFlash(false);
     setFlashContent(flashContent);
     setShowFlash(true);
   };
+  const updateUser = (user) => {
+    setAppState({
+      user: user,
+      updateUser,
+    });
+  };
+  const [appState, setAppState] = useState({
+    user: {
+      name: "Timmy",
+      email: "tim@soundtemple.com.au",
+      username: "soundtemple",
+    },
+    updateUser,
+  });
   return (
-    <div className="App">
-      <MuiThemeProvider theme={theme}>
-        {showFlash && <FlashMessage {...flashContent} />}
-        <div>
-          <CssBaseline />
-          <Dashboard onFlash={handleFlashMessage} />
-        </div>
-      </MuiThemeProvider>
-    </div>
+    <AppContext.Provider value={appState}>
+      <div className="App">
+        <MuiThemeProvider theme={theme}>
+          {showFlash && <FlashMessage {...flashContent} />}
+          <div>
+            <CssBaseline />
+            <Dashboard onFlash={handleFlashMessage} />
+          </div>
+        </MuiThemeProvider>
+      </div>
+    </AppContext.Provider>
   );
 };
 
--- frontend/src/general/components/home.js
+++ frontend/src/general/components/home.js
@@ -1,13 +1,23 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
+import AppContext from "../../app/components/app-context";
 
 const HomePage = (props) => {
+  const { user, updateUser } = React.useContext(AppContext);
   return (
     <>
       <h2>Latest from Soundtemple</h2>
+      <p>Hey {user.name},</p>
       <p>Some stuff in the news</p>
       <p>Some new music</p>
       <p>Some new software</p>
+      <p>Your username: {user.email}</p>
+      <button
+        onClick={(user) =>
+          updateUser({ name: "Bob", email: "bob@cat.com", username: "bobcat" })
+        }
+      >
+        Update User
+      </button>
     </>
   );
 };
